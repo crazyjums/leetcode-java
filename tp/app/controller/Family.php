9b74a10828d3ea800b9273a82c6967edacc4e5dd
crazyjums <crazyjums@gmail.com>
2021-07-17 17:07:05 +0800

    第一次提交 thinkphp

---
+++ tp/app/controller/Family.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+
+class FamilyController extends BaseController
+{
+    public function test() {
+        var_dump(__FUNCTION__);
+    }
+}
